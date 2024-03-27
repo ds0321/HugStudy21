e8b46ccf4dfd981d90df160dd4ba92c646aff763
ds0321 <159526458+ds0321@users.noreply.github.com>
2024-03-27 19:52:26 +0900

    4-20

--- Study21/springSample/src/main/java/com/example/demo/controller/UserController.java
+++ Study21/springSample/src/main/java/com/example/demo/controller/UserController.java
@@ -1,30 +1,33 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dto.UserRequest;
 import com.example.demo.entity.UserEntity;
 import com.example.demo.service.UserService;
-
 /**
  * ユーザー情報 Controller
  */
 @Controller
 public class UserController {
 
-
   /**
    * ユーザー情報 Service
    */
   @Autowired
   UserService userService;
-
 
   /**
    * ユーザー情報一覧画面を表示
@@ -38,6 +41,7 @@ public class UserController {
     model.addAttribute("userlist", userlist);
     return "user/list";
   }
+
   /**
    * ユーザー新規登録画面を表示
    * @param model Model
@@ -45,7 +49,31 @@ public class UserController {
    */
   @RequestMapping("/user/add")
   public String userRegister(Model model) {
+    model.addAttribute("userRequest", new UserRequest());
     return "user/add";
+  }
+
+  /**
+   * ユーザー新規登録
+   * @param userRequest リクエストデータ
+   * @param model Model
+   * @return ユーザー情報一覧画面
+   */
+  @RequestMapping("/user/create")
+  public String userCreate(@Validated @ModelAttribute UserRequest userRequest, BindingResult result, Model model) {
+    if (result.hasErrors()) {
+      // 入力チェックエラーの場合
+      List<String> errorList = new ArrayList<String>();
+      for (ObjectError error : result.getAllErrors()) {
+        errorList.add(error.getDefaultMessage());
+      }
+      //エラー判定後の画面遷移
+      model.addAttribute("validationError", errorList);
+      return "user/add";
+    }
+    // ユーザー情報の登録
+    userService.create(userRequest);
+    return "redirect:/user/list";
   }
 
   /**
@@ -56,7 +84,10 @@ public class UserController {
    */
   @GetMapping("/user/{id}")
   public String userDetail(@PathVariable Integer id, Model model) {
+    UserEntity user = userService.findById(id);
+    model.addAttribute("userData", user);
     return "user/view";
   }
+  
 
 }
